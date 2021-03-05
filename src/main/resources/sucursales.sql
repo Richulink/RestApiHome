d07c981651bd2f0ea8e314a436431ec28930ba59
Richard <71940232+Richulink@users.noreply.github.com>
2021-03-05 16:16:26 -0300

    Update sucursales.sql
    agregado de sucursales

--- src/main/resources/sucursales.sql
+++ src/main/resources/sucursales.sql
@@ -1,11 +1,11 @@
-insert into sucursales (cuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
+insert into sucursales (clientNumberCuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
 values (3152367,'dorrego 2246','farmatily','andrea','monica','Mardelplata','Bsas','Argentina',1);
 
-insert into sucursales (cuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
+insert into sucursales (clientNumberCuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
 values (5432452,'Luro 4356','fava','roberto funcini','jorge magala','Mardelplata','Bsas','Argentina',1);
 
-insert into sucursales (cuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
+insert into sucursales (clientNumberCuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
 values (765467,'Jujuy 0989','farmatily','magali zarsa','osvaldo perré','Mardelplata','Bsas','Argentina',1);
 
-insert into sucursales (cuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
+insert into sucursales (clientNumberCuit,location,businessName,managerName,subManagerName,city,Province,country,onHold )
 values (9546,'JPaso 0989','Montecarlo','antonela miranda','florencia pascual','Mardelplata','Bsas','Argentina',1);
