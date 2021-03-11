116bbe6291b54052c80a5d52e88e8f489b2474da
Richard <71940232+Richulink@users.noreply.github.com>
2021-03-10 22:31:06 -0300

    reduccion y mejora del codigo

--- src/main/java/com/example/apiHome/domain/Producto.java
+++ src/main/java/com/example/apiHome/domain/Producto.java
@@ -5,7 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.*;;
+import javax.persistence.*;
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -17,12 +18,21 @@ import java.util.Date;
 @Builder
 @Component
 public class Producto implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long idUser;
+    private Long idProducto;
+    @Column(nullable = true)
+    private int codigoProducto;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(unique = true)
     private String nombre;
+
     private String producto;
     private String catalogo;
     private Boolean stock;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,32 @@
-insert into productos(nombre,producto,catalogo,fecha_alta)   values ('nike','zapatillas','calzado',NOW());
-insert into productos(nombre,producto,catalogo,fecha_alta)   values ('puma','zapatillas','calzado',NOW());
-insert into productos(nombre,producto,catalogo,fecha_alta)   values ('topper','zapatillas','calzado',NOW());
-insert into productos(nombre,producto,catalogo,fecha_alta)   values ('philco','tv','smart',NOW());
-insert into productos(nombre,producto,catalogo,fecha_alta)   values ('aiwa','minicomponente','electrodomesticos',NOW());
+insert into productos(nombre,producto,catalogo,fecha_alta,precio)
+              values ('nike','zapatillas','calzado',NOW(),00.0);
+
+insert into productos(nombre,producto,catalogo,fecha_alta,precio)
+              values ('puma','zapatillas','calzado',NOW(),00.0);
+
+insert into productos(nombre,producto,catalogo,fecha_alta,precio)
+              values ('topper','zapatillas','calzado',NOW(),00.0);
+
+insert into productos(nombre,producto,catalogo,fecha_alta,precio)
+              values ('philco','tv','smart',NOW(),00.0);
+
+insert into productos(nombre,producto,catalogo,fecha_alta,precio)
+              values ('aiwa','minicomponente','electrodomesticos',NOW(),00.0);
+
+insert into sucursales (country,city,province,business_name,manager_name,submanager_name,cuit,location,on_hold )
+                values ('argentina','mardelplata','bsas','playaclub','ricardo','victor',45654,'jjpaso 5325',1);
+
+insert into sucursales (country,city,province,business_name,manager_name,submanager_name,cuit,location,on_hold )
+                values ('argentina','miramar','bsas','sancor seguros','marcelo','vanesa',32423,'calle 34 98005',1);
+
+insert into sucursales (country,city,province,business_name,manager_name,submanager_name,cuit,location,on_hold )
+                values ('argentina','pinarmar','bsas','zapateria punta mogotes','julian','alfredo',62245,'calle 54 76534',1);
+
+insert into sucursales (country,city,province,business_name,manager_name,submanager_name,cuit,location,on_hold )
+                values ('argentina','mardelplata','bsas','party','ana','elena',76546,'alvarado 2348',1);
+
+insert into sucursales (country,city,province,business_name,manager_name,submanager_name,cuit,location,on_hold )
+                values ('argentina','mardelplata','bsas','la rioja','oscar','laura',32123234,'moron 26266',1);
+
+insert into sucursales (country,city,province,business_name,manager_name,submanager_name,cuit,location,on_hold )
+                values ('argentina','mar del plata','bsas','radatily','jorge','hector',462654,'colon 2348',1);
